047beff027de957398ef18efce09a67bbc8a2ab4
Thnbr <thiago.brazil.ribeiro@escola.pr.gov.br>
2025-05-29 07:56:11 -0300

    jgu

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'História',
+    'Quem foi a primeira pessoa a viajar pelo espaço?',
+    'Yuri Gargarin'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos bem definidos para resolver um problema ou realizar uma tarefa. '
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz Quantos anos você tem Inglês?',
+    'Quantos anos você tem? é How old are you? (RAI)'
 )
